75ea657efea15b6dd683f8a2c233e44d021fa76e
Marianne Hval <marianne.hval@nav.no>
2020-12-18 13:50:14 +0100

    Min height so footer doesnt expand

--- src/styled-components/main-container.tsx
+++ src/styled-components/main-container.tsx
@@ -4,6 +4,7 @@ export const InngangMainContainer = styled.main`
     padding-bottom: 128px;
     padding-top: 64px;
     background-color: #e7e9e9;
+    min-height: 75vh;
 `;
 
 export const FormMainContainer = styled.main`
